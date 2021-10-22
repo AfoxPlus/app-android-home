a7c395c1f0f3ec7552932dc49ba8a4139323a009
afoxpluss <92241438+afoxpluss@users.noreply.github.com>
2021-10-21 20:14:51 -0500

    Initial commit

---
+++ buildSrc/src/main/kotlin/ConfigureApp.kt
@@ -0,0 +1,13 @@
+object ConfigureApp {
+    //TODO change urlRepoDependencies with your urlRepoDependencies name
+    const val urlRepoDependencies = "https://maven.pkg.github.com/afoxplus/app-android-module"
+    //TODO change organization with your organization name
+    const val organization = "afoxplus"
+    //TODO change applicationId with your module name
+    const val applicationId = "app-android-module"
+    //TODO change groupId with your groupId name
+    const val groupId = "com.afoxplus.android"
+    //TODO change artifactId with your module name
+    const val artifactId = "module"
+    const val version = "1.0.0"
+}
---
+++ module/build.gradle.kts
@@ -0,0 +1,69 @@
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+    id("kotlin-kapt")
+    id("kotlin-parcelize")
+    id("maven-publish")
+}
+
+apply(from = "sonarqube.gradle")
+apply(from = "jacoco.gradle")
+apply(from = "upload.gradle")
+
+android {
+    compileSdk = VersionApp.compileSdkVersion
+    buildToolsVersion = VersionApp.buildToolsVersion
+
+    defaultConfig {
+        minSdk = VersionApp.minSdkVersion
+        targetSdk = VersionApp.targetSdkVersion
+        testInstrumentationRunner = VersionApp.testInstrumentationRunner
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            buildConfigField("String", "EXAMPLE_FIELD", "\"example-release\"")
+        }
+
+        getByName("debug") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            buildConfigField("String", "EXAMPLE_FIELD", "\"example-debug\"")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions.jvmTarget = JavaVersion.VERSION_11.toString()
+
+    buildFeatures { viewBinding = true }
+
+    lint {
+        isCheckDependencies = true
+    }
+}
+
+dependencies {
+    implementation(fileTree("libs") { include(listOf("*.jar", "*.aar")) })
+    implementation(MainApplicationDependencies.kotlinStdlib)
+    implementation(MainApplicationDependencies.coreKtx)
+    implementation(MainApplicationDependencies.appCompat)
+    implementation(MainApplicationDependencies.material)
+    implementation(MainApplicationDependencies.constraintLayout)
+
+    testImplementation(TestDependencies.junit)
+    androidTestImplementation(TestDependencies.extJUnit)
+    androidTestImplementation(TestDependencies.espressoCore)
+}
