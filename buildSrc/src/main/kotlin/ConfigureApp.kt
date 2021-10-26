34995132260981a5cb15989ec9dfefcd8edca9ff
Carlos Osorio <hacybeyker@gmail.com>
2021-10-25 19:46:24 -0500

    se modificaron las configuraciones del proyecto

--- buildSrc/src/main/kotlin/ConfigureApp.kt
+++ buildSrc/src/main/kotlin/ConfigureApp.kt
@@ -1,13 +1,9 @@
 object ConfigureApp {
-    //TODO change urlRepoDependencies with your urlRepoDependencies name
-    const val urlRepoDependencies = "https://maven.pkg.github.com/afoxplus/app-android-module"
-    //TODO change organization with your organization name
+    const val urlRepoDependencies = "https://maven.pkg.github.com/afoxplus/app-yalisto-android"
     const val organization = "afoxplus"
-    //TODO change applicationId with your module name
-    const val applicationId = "app-android-module"
-    //TODO change groupId with your groupId name
+    const val projectName = "app-android-home"
+    const val applicationId = "app.android.home"
     const val groupId = "com.afoxplus.android"
-    //TODO change artifactId with your module name
-    const val artifactId = "module"
+    const val artifactId = "home"
     const val version = "1.0.0"
 }
