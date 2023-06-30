1315260a3eaff6c4937b51a24b68c4bf4b5b4f03
Carlos Osorio <hacybeyker@gmail.com>
2023-06-30 14:18:50 -0500

    se agrego estado de ordenes (#18)

--- demo/src/main/java/com/afoxplus/home/demo/global/AppPropertiesDemo.kt
+++ demo/src/main/java/com/afoxplus/home/demo/global/AppPropertiesDemo.kt
@@ -5,8 +5,16 @@ import com.afoxplus.network.global.AppProperties
 import javax.inject.Inject
 
 class AppPropertiesDemo @Inject constructor() : AppProperties {
+    override fun getCurrencyID(): String {
+        return ""
+    }
+
     override fun getDeviceData(): String {
         return "home_demo"
+    }
+
+    override fun getUserUUID(): String {
+        return ""
     }
 
     override fun isAppDebug(): Boolean {
