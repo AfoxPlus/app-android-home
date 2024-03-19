569c425cb760d03a4b13a55d5d90a2024fc521aa
Valentin Mendoza Torres <jmendozat13@gmail.com>
2024-03-19 15:43:48 -0500

    changes (#31) (#32)
    Co-authored-by: Carlos Osorio <hacybeyker@gmail.com>

--- demo/src/main/java/com/afoxplus/home/demo/global/HomeStartDemoFlow.kt
+++ demo/src/main/java/com/afoxplus/home/demo/global/HomeStartDemoFlow.kt
@@ -2,7 +2,7 @@ package com.afoxplus.home.demo.global
 
 import android.content.Intent
 import com.afoxplus.home.demo.delivery.views.activites.MainActivity
-import com.afoxplus.module.delivery.flow.StartDemoFlow
+import com.afoxplus.demo_config.delivery.flow.StartDemoFlow
 import com.afoxplus.uikit.activities.UIKitBaseActivity
 import javax.inject.Inject
 
--- module/build.gradle.kts
+++ module/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("com.android.library") version "7.3.1"
-    id("org.jetbrains.kotlin.android") version "1.7.20"
-    id("org.jetbrains.kotlin.kapt") version "1.7.20"
+    id("com.android.library") version "8.0.2"
+    id("org.jetbrains.kotlin.android") version "1.8.22"
+    id("org.jetbrains.kotlin.kapt") version "1.8.22"
     id("com.google.dagger.hilt.android") version "2.44.2"
     id("org.jetbrains.kotlin.plugin.parcelize") version "1.7.20"
     id("org.sonarqube") version "3.3"
@@ -12,6 +12,7 @@ apply {
     from("sonarqube.gradle")
     from("jacoco.gradle")
     from("upload.gradle")
+    from("graph.gradle.kts")
 }
 
 android {
@@ -54,13 +55,21 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions.jvmTarget = JavaVersion.VERSION_11.toString()
+    kotlinOptions.jvmTarget = JavaVersion.VERSION_17.toString()
 
-    buildFeatures { viewBinding = true }
+    buildFeatures {
+        viewBinding = true
+        dataBinding = true
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.8"
+    }
 
     lint {
         disable.addAll(
@@ -92,48 +101,50 @@ android {
 
 dependencies {
     implementation(fileTree("libs") { include(listOf("*.jar", "*.aar")) })
-
+    implementation(Deps.Jetpack.kotlin)
     implementation(Deps.Jetpack.core)
     implementation(Deps.Jetpack.appcompat)
-
-    implementation(Deps.Jetpack.activity)
     implementation(Deps.Jetpack.fragment)
-
     implementation(Deps.UI.materialDesign)
     implementation(Deps.UI.constraintLayout)
 
-    implementation(Deps.UI.glide)
-    kapt(Deps.UI.glideCompiler)
+    // JetpackCompose
+    implementation(Deps.JetpackCompose.activity)
+    implementation(Deps.JetpackCompose.constraintlayout)
+    implementation(Deps.JetpackCompose.navigation)
+    implementation(platform(Deps.JetpackCompose.bom))
+    implementation(Deps.JetpackCompose.ui)
+    implementation(Deps.JetpackCompose.graphics)
+    implementation(Deps.JetpackCompose.toolingPreview)
+    implementation(Deps.JetpackCompose.material3)
+    implementation(Deps.JetpackCompose.materialIconExtended)
 
-    implementation(Deps.Arch.network)
+    // External Libraries
+    implementation(Deps.Arch.hiltAndroid)
+    kapt(Deps.Arch.hiltCompiler)
+    implementation(Deps.Arch.coroutinesCore)
     implementation(Deps.Arch.retrofit2)
     implementation(Deps.Arch.loggingInterceptor)
     implementation(Deps.Arch.gson)
-
-    implementation(Deps.Arch.coroutinesCore)
-
-    implementation(Deps.Arch.hiltAndroid)
-    kapt(Deps.Arch.hiltCompiler)
-
+    implementation(Deps.UI.glide)
+    kapt(Deps.UI.glideCompiler)
     implementation(Deps.Arch.zxingAndroid) { isTransitive = false }
     implementation(Deps.Arch.zxingCore)
 
+    // Test
     testImplementation(Deps.Test.jUnit)
     androidTestImplementation(Deps.Test.androidJUnit)
     androidTestImplementation(Deps.Test.espresso)
+    testImplementation(Deps.Test.mockito)
+    testImplementation(Deps.Test.mockitoKotlin)
+    testImplementation(Deps.Test.mockitoInline)
+    testImplementation(Deps.Test.testCore)
+    testImplementation(Deps.Test.kotlinCoroutine)
 
-    testImplementation("org.mockito:mockito-core:4.3.1")
-    testImplementation("org.mockito.kotlin:mockito-kotlin:4.0.0")
-    testImplementation("org.mockito:mockito-inline:4.3.1")
-
-    testImplementation("androidx.arch.core:core-testing:2.2.0")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.0")
-
-    //Business Dependencies
+    // Business Dependencies
+    implementation(Deps.UI.uikit)
+    implementation(Deps.Arch.network)
     implementation(Deps.Arch.products)
     implementation(Deps.Arch.restaurants)
     implementation(Deps.Arch.orders)
-    implementation(Deps.UI.uikit)
-
-    LocalModules.setupBuildGradle(this, rootProject,"home")
 }
