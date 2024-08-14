af9f8dab5b792043588d7789ec00cd62eb80caeb
Valentin Mendoza Torres <jmendozat13@gmail.com>
2024-08-14 14:47:52 -0500

    Fix lazy column (#45)

--- module/src/main/java/com/afoxplus/home/delivery/screens/HomeScreen.kt
+++ module/src/main/java/com/afoxplus/home/delivery/screens/HomeScreen.kt
@@ -1,9 +1,11 @@
 package com.afoxplus.home.delivery.screens
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.FloatingActionButton
@@ -49,14 +51,15 @@ internal fun HomeScreen(
             })
         },
         content = {
-            LazyColumn(
-                state= rememberLazyListState(),
-                contentPadding = it,
-                modifier = Modifier.fillMaxSize()
+            Column(
+                modifier = Modifier
+                    .verticalScroll(rememberScrollState())
+                    .fillMaxSize()
+                    .padding(it)
             ) {
-                item { bannerInvitationContent() }
-                item { ordersStatusContent() }
-                item { restaurantsContent() }
+                bannerInvitationContent()
+                ordersStatusContent()
+                restaurantsContent()
             }
         }
     )
